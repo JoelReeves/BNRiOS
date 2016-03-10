0edd2b3d87332f0ae24e70e729f011fa03bebb43
Joel Reeves <jreeves@tonicdesign.com>
2016-03-10 15:42:35 -0500

    optionals

--- Chapter_02/StandardTypes.playground/Contents.swift
+++ Chapter_02/StandardTypes.playground/Contents.swift
@@ -45,3 +45,18 @@ let floatFromLiteral = Float(3.14)
 let easyPi = 3.14 // defaults to Double
 let floatFromDouble = Float(easyPi)
 let floatingPi: Float = 3.14
+
+var reading1: Float?
+var reading2: Float?
+var reading3: Float?
+
+reading1 = 9.8
+reading2 = 9.2
+reading3 = 9.7
+
+//let avgReading = (reading1! + reading2! + reading3!) / 3 // dangerous because program will crash if any readings are nil
+if let r1 = reading1, r2 = reading2, r3 = reading3 {
+    let avgReading = (r1 + r2 + r3) / 3
+} else {
+    let errorString = "Instrument reported a reading that was nil"
+}
