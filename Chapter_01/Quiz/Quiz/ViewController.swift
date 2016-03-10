5782b0636314e50035136f06c691e6f0c461b906
Joel Reeves <jreeves@tonicdesign.com>
2016-03-10 14:42:05 -0500

    creating model layer - arrays of questions and answers, and variable to keep track of the current question

--- Chapter_01/Quiz/Quiz/ViewController.swift
+++ Chapter_01/Quiz/Quiz/ViewController.swift
@@ -13,6 +13,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var answerLabel: UILabel!
     
+    let questions: [String] = ["From what is cognac made?",
+                                "What is 7+7?",
+                                "What is the capital of Vermont?"]
+    
+    let answers: [String] = ["Grapes", "14", "Montpelier"]
+    
+    var currentQuestionIndex: Int = 0
+    
     @IBAction func showNextQuestion(sender: UIButton) {
         
     }
