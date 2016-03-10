775aa33dc00a57f51f8f65c0988ff8b1a198dd0b
Joel Reeves <jreeves@tonicdesign.com>
2016-03-10 15:17:50 -0500

    using standard types

---
+++ Chapter_02/StandardTypes.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var str = "Hello, playground"
+str = "Hello, Swift"
+let constStr = str
+
+var nextYear: Int
+var bodyTemp: Float
+var hasPet: Bool
+
+var arrayOfInts: [Int]
+
+var dictionaryOfCapitalsByCountry: [String:String]
+
+var winningLotteryNumbers: Set<Int>
