86f204e1b119a89f9f755732162baadc3d91be03
Joel Reeves <jreeves@tonicdesign.com>
2016-03-10 16:22:57 -0500

    adding and customizing labels

---
+++ Chapter_03/WorldTrotter/WorldTrotter/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Joel Reeves on 3/10/16.
+//  Copyright © 2016 Bromance Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+}
+
