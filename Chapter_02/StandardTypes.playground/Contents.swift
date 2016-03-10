6100d8abf871ae4d525490aeafc8b159529331bf
Joel Reeves <jreeves@tonicdesign.com>
2016-03-10 15:35:12 -0500

    using instance methods on a type

--- Chapter_02/StandardTypes.playground/Contents.swift
+++ Chapter_02/StandardTypes.playground/Contents.swift
@@ -17,12 +17,14 @@ var winningLotteryNumbers: Set<Int>
 let number = 42
 let fmStation = 91.1
 
-let countingUp = ["one", "two"]
+var countingUp = ["one", "two"]
 let nameByParkingSpace = [13: "Alice", 27: "Bob"]
 
 let secondElement = countingUp[1]
 
 countingUp.count
+
+countingUp.append("three")
 
 let emptyString = String()
 emptyString.isEmpty
