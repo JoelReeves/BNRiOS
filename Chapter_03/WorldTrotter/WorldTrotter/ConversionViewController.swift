d30ae0cbe984507892776229b21b91e26306abe0
Joel Reeves <jreeves@tonicdesign.com>
2016-03-11 11:10:55 -0500

    deleting original ViewController and creating new ConversionViewController

---
+++ Chapter_03/WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Joel Reeves on 3/11/16.
+//  Copyright © 2016 Bromance Labs. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+    
+}
