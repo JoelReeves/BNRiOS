5c8066b351893c05b3ef98198654d4040efcae3f
Joel Reeves <jreeves@tonicdesign.com>
2016-03-11 16:16:46 -0500

    adding new view controller to the tab bar controller that will display a webview

---
+++ Chapter_03/WorldTrotter/WorldTrotter/WebViewController.swift
@@ -0,0 +1,14 @@
+//
+//  WebViewController.swift
+//  WorldTrotter
+//
+//  Created by Joel Reeves on 3/11/16.
+//  Copyright © 2016 Bromance Labs. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class WebViewController: UIViewController {
+    
+}
