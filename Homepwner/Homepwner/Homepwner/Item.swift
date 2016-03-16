6ee0324257b89c3d0ee93275608adce592c8cf13
Joel Reeves <jreeves@tonicdesign.com>
2016-03-16 09:34:51 -0400

    creating Item class with an initializer

---
+++ Homepwner/Homepwner/Homepwner/Item.swift
@@ -0,0 +1,25 @@
+//
+//  Item.swift
+//  Homepwner
+//
+//  Created by Joel Reeves on 3/16/16.
+//  Copyright © 2016 Bromance Labs. All rights reserved.
+//
+
+import UIKit
+
+class Item: NSObject {
+    var name: String
+    var valueInDollars: Int
+    var serialNumber: String?
+    let dateCreated: NSDate
+    
+    init(name: String, serialNumber: String?, valueInDollars: Int) {
+        self.name = name
+        self.valueInDollars = valueInDollars
+        self.serialNumber = serialNumber
+        self.dateCreated = NSDate()
+        
+        super.init()
+    }
+}
