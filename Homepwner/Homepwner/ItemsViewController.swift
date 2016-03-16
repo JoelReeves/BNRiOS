382a71c6edf4095789fdb2c3eba5ac2492afd773
Joel Reeves <jreeves@tonicdesign.com>
2016-03-16 10:13:31 -0400

    implementing method so that the nth row displays the nth entry in the allItems array

--- Homepwner/Homepwner/ItemsViewController.swift
+++ Homepwner/Homepwner/ItemsViewController.swift
@@ -16,4 +16,18 @@ class ItemsViewController: UITableViewController {
         return itemStore.allItems.count
     }
     
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // create an instance of UITableViewCell with a default appearance
+        let cell = UITableViewCell(style: .Value1, reuseIdentifier: "UITableViewCell")
+        
+        // set the text on the cell with the description of the item that is at the nth index of items, where n = row
+        // this cell will appear in on the tableview
+        let item = itemStore.allItems[indexPath.row]
+        
+        cell.textLabel?.text = item.name
+        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        
+        return cell
+    }
+    
 }
