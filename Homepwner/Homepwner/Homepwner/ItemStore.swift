1cc3c44c35461bbf92e088ca6b79cf4c7c7427a9
Joel Reeves <jreeves@tonicdesign.com>
2016-03-16 09:49:53 -0400

    defining ItemStore class to store an array of Items

---
+++ Homepwner/Homepwner/Homepwner/ItemStore.swift
@@ -0,0 +1,13 @@
+//
+//  ItemStore.swift
+//  Homepwner
+//
+//  Created by Joel Reeves on 3/16/16.
+//  Copyright © 2016 Bromance Labs. All rights reserved.
+//
+
+import UIKit
+
+class ItemStore {
+    var allItems = [Item]()
+}
