e5f6d73528f4540f8731cfd4a4486cffe6dc7694
Joel Reeves <jreeves@tonicdesign.com>
2016-03-21 12:27:20 -0400

    making sure the last row of the UITableView always says "No More Items!"

--- Homepwner/Homepwner/ItemsViewController.swift
+++ Homepwner/Homepwner/ItemsViewController.swift
@@ -37,32 +37,62 @@ class ItemsViewController: UITableViewController {
         
         itemsArray.append(itemsLessThan50)
         itemsArray.append(itemsMoreThan50)
+        
+        print(itemsArray)
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemsArray[section].count
+        if itemsArray.isEmpty {
+            return 1
+        } else {
+            return itemsArray[section].count
+        }
     }
     
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return itemsArray.count
+        if itemsArray.isEmpty {
+            return 1
+        } else {
+            return itemsArray.count
+        }
     }
     
     override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return sectionTitles[section]
+        if itemsArray.isEmpty {
+            return nil
+        } else {
+            return sectionTitles[section]
+        }
+    }
+    
+    override func tableView(tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        if section == sectionTitles.count - 1 {
+            return "No More Items!"
+        } else {
+            return ""
+        }
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        // get a new or recycled cell
-        let cell = tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath)
+        var cell: UITableViewCell?
         
-        // set the text on the cell with the description of the item that is at the nth index of items, where n = row
-        // this cell will appear in on the tableview
-        let item = itemsArray[indexPath.section][indexPath.row]
+        if itemsArray.isEmpty {
+            cell = UITableViewCell(style: .Value1, reuseIdentifier: "UITableViewCell")
+            cell!.textLabel?.text = "No More Items"
+            cell?.detailTextLabel?.text = ""
+        } else {
+            // get a new or recycled cell
+            cell = tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath)
+            
+            // set the text on the cell with the description of the item that is at the nth index of items, where n = row
+            // this cell will appear in on the tableview
+            let item = itemsArray[indexPath.section][indexPath.row]
+            
+            cell!.textLabel?.text = item.name
+            cell!.detailTextLabel?.text = "$\(item.valueInDollars)"
+        }
         
-        cell.textLabel?.text = item.name
-        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
-        
-        return cell
+        return cell!
     }
     
 }
