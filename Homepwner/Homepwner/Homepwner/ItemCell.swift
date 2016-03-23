906846e9c6c49f87b1cb6bb7c0264bbdb1770c81
Joel Reeves <jreeves@tonicdesign.com>
2016-03-23 12:20:15 -0400

    connecting IBOutlets to labels

--- Homepwner/Homepwner/Homepwner/ItemCell.swift
+++ Homepwner/Homepwner/Homepwner/ItemCell.swift
@@ -10,4 +10,8 @@ import UIKit
 
 class ItemCell: UITableViewCell {
     
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var serialNumberLabel: UILabel!
+    @IBOutlet weak var valueLabel: UILabel!
+    
 }
