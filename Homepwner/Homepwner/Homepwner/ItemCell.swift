29f48b7477fe34950802c83db951c66e5625d2ed
Joel Reeves <jreeves@tonicdesign.com>
2016-03-23 12:11:39 -0400

    creating ItemCell class

---
+++ Homepwner/Homepwner/Homepwner/ItemCell.swift
@@ -0,0 +1,13 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Joel Reeves on 3/23/16.
+//  Copyright © 2016 Bromance Labs. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+}
