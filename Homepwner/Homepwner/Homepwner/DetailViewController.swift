8c35b7a745c0782c332ae69aff1454bb2f15fe0e
Joel Reeves <jreeves@tonicdesign.com>
2016-03-24 10:35:50 -0400

    creating IBOutlets for DetailViewController

---
+++ Homepwner/Homepwner/Homepwner/DetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.swift
+//  Homepwner
+//
+//  Created by Joel Reeves on 3/24/16.
+//  Copyright © 2016 Bromance Labs. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet var nameField: UITextField!
+    @IBOutlet var serialNumberField: UITextField!
+    @IBOutlet var valueField: UITextField!
+    @IBOutlet var dateLabel: UILabel!
+}
