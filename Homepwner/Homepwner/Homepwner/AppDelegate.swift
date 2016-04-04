8cd0335e82620d988cee2f1ba4fe72501a82c4a7
Joel Reeves <jreeves@tonicdesign.com>
2016-04-04 09:54:55 -0400

    making itemStore variable global in class; using applicationDidEnterBackground to save Item instances

--- Homepwner/Homepwner/Homepwner/AppDelegate.swift
+++ Homepwner/Homepwner/Homepwner/AppDelegate.swift
@@ -12,13 +12,10 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    let itemStore = ItemStore()
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        
-        // creating an ItemStore
-        let itemStore = ItemStore()
         
         // create an ImageStore
         let imageStore = ImageStore()
@@ -38,8 +35,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        let success = itemStore.saveChanges()
+        
+        if success {
+            print("saved all items")
+        } else {
+            print("couldn't save any of the items")
+        }
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
