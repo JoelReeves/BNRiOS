fe7277d588781fda4e739549920765b18d9321bd
Joel Reeves <jreeves@tonicdesign.com>
2016-04-04 11:43:33 -0400

    initial project setup

---
+++ TouchTracker/TouchTracker/TouchTracker/DrawView.swift
@@ -0,0 +1,15 @@
+//
+//  DrawView.swift
+//  TouchTracker
+//
+//  Created by Joel Reeves on 4/4/16.
+//  Copyright © 2016 Bromance Labs. All rights reserved.
+//
+
+import UIKit
+
+class DrawView: UIView {
+    
+    var currentLine: Line?
+    var finishedLines = [Line]()
+}
---
+++ TouchTracker/TouchTracker/TouchTracker/Line.swift
@@ -0,0 +1,16 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Joel Reeves on 4/4/16.
+//  Copyright © 2016 Bromance Labs. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
