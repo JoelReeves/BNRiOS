bbbab00c451850ffb5f025625803d2c56adb9e65
Joel Reeves <jreeves@tonicdesign.com>
2016-04-05 12:32:00 -0400

    initial setup

---
+++ Photorama/Photorama/Photorama/PhotosViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Joel Reeves on 4/5/16.
+//  Copyright © 2016 Bromance Labs. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+}
