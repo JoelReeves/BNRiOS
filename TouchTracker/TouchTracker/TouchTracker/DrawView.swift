456632553dbddc4c5bbc9fe853518326a45884c0
Joel Reeves <jreeves@tonicdesign.com>
2016-04-05 09:10:36 -0400

    implementing methods for stroking lines and overhang drawRect

--- TouchTracker/TouchTracker/TouchTracker/DrawView.swift
+++ TouchTracker/TouchTracker/TouchTracker/DrawView.swift
@@ -12,4 +12,29 @@ class DrawView: UIView {
     
     var currentLine: Line?
     var finishedLines = [Line]()
+    
+    func strokeLine(line: Line) {
+        let path = UIBezierPath()
+        path.lineWidth = 10
+        path.lineCapStyle = CGLineCap.Round
+        
+        path.moveToPoint(line.begin)
+        path.addLineToPoint(line.end)
+        path.stroke()
+    }
+    
+    override func drawRect(rect: CGRect) {
+        // drawing finished lines in black
+        UIColor.blackColor().setStroke()
+        
+        for line in finishedLines {
+            strokeLine(line)
+        }
+        
+        if let line = currentLine {
+            // if there is a line currently being drawn, do it in red
+            UIColor.redColor().setStroke()
+            strokeLine(line)
+        }
+    }
 }
