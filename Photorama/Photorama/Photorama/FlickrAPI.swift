762603e5552d2e7a3dfeb21fafb1fabbbfd70c52
Joel Reeves <jreeves@tonicdesign.com>
2016-04-06 08:58:08 -0400

    creating struct to be used with the Flickr API

---
+++ Photorama/Photorama/Photorama/FlickrAPI.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrAPI.swift
+//  Photorama
+//
+//  Created by Joel Reeves on 4/6/16.
+//  Copyright © 2016 Bromance Labs. All rights reserved.
+//
+
+import Foundation
+
+enum Method: String {
+    case RecentPhotos = "flickr.photos.getRecent"
+}
+
+struct FlickrAPI {
+    private static let baseURLString = "https://api.flickr.com/services/rest"
+}
