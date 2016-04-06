97d10fcddb639f498f1d5bb5140cb95050b94090
Joel Reeves <jreeves@tonicdesign.com>
2016-04-06 09:27:11 -0400

    creating class to handle fetching recent Flickr photos and downloading the image data for each photo

---
+++ Photorama/Photorama/Photorama/PhotoStore.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoStore.swift
+//  Photorama
+//
+//  Created by Joel Reeves on 4/6/16.
+//  Copyright © 2016 Bromance Labs. All rights reserved.
+//
+
+import Foundation
+
+class PhotoStore {
+    
+}
