40c8cd5cdad6812cb46286d6debc579e51f0487c
Joel Reeves <jreeves@tonicdesign.com>
2016-04-07 10:18:59 -0400

    using methods to reset each cell to the spinning stale when the cell is first created and when the cell is getting reused

--- Photorama/Photorama/Photorama/PhotoCollectionViewCell.swift
+++ Photorama/Photorama/Photorama/PhotoCollectionViewCell.swift
@@ -13,6 +13,18 @@ class PhotoCollectionViewCell: UICollectionViewCell {
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var spinner: UIActivityIndicatorView!
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        updateWithImage(nil)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        updateWithImage(nil)
+    }
+    
     func updateWithImage(image: UIImage?) {
         if let imageToDisplay = image {
             spinner.stopAnimating()
