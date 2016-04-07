8f824ebe38f87ba8806a305075e68f84eabe172b
Joel Reeves <jreeves@tonicdesign.com>
2016-04-07 11:20:19 -0400

    adding new view controller so that when a photo is tapped, you will navigate to look at that photo

---
+++ Photorama/Photorama/Photorama/PhotoInfoViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoInfoViewController.swift
+//  Photorama
+//
+//  Created by Joel Reeves on 4/7/16.
+//  Copyright © 2016 Bromance Labs. All rights reserved.
+//
+
+import UIKit
+
+class PhotoInfoViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+}
