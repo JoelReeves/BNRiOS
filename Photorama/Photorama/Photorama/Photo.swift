c0dac12eda988513fdb6dd05e14dd8602b61b0e1
Joel Reeves <jreeves@tonicdesign.com>
2016-04-08 09:28:10 -0400

    created Photo entity with initial values set

--- Photorama/Photorama/Photorama/Photo.swift
+++ Photorama/Photorama/Photorama/Photo.swift
@@ -6,12 +6,25 @@
 //  Copyright © 2016 Bromance Labs. All rights reserved.
 //
 
-import Foundation
+import UIKit
 import CoreData
 
 
 class Photo: NSManagedObject {
 
 // Insert code here to add functionality to your managed object subclass
+    
+    var image: UIImage?
+    
+    override func awakeFromInsert() {
+        super.awakeFromInsert()
+        
+        // giving properties thier initial values
+        title = ""
+        photoID = ""
+        remoteURL = NSURL()
+        photoKey = NSUUID().UUIDString
+        dateTaken = NSDate()
+    }
 
 }
