6b2e99271df5b99341702f6f9f8e742dd1dea4fa
Joel Reeves <jreeves@tonicdesign.com>
2016-04-08 11:29:26 -0400

    adding table view delegate methods and adding the appropriate index paths to the selectedIndexPaths array

--- Photorama/Photorama/Photorama/TagsViewController.swift
+++ Photorama/Photorama/Photorama/TagsViewController.swift
@@ -22,6 +22,7 @@ class TagsViewController: UITableViewController {
         super.viewDidLoad()
         
         tableView.dataSource = tagsDataSource
+        tableView.delegate = self
         
         updateTags()
     }
@@ -29,5 +30,32 @@ class TagsViewController: UITableViewController {
     func updateTags() {
         let tags = try! store.fetchMainQueueTags(predicate: nil, sortDescriptors: [NSSortDescriptor(key: "name", ascending: true)])
         tagsDataSource.tags = tags
+        
+        for tag in photo.tags {
+            if let index = tagsDataSource.tags.indexOf(tag) {
+                let indexPath = NSIndexPath(forRow: index, inSection: 0)
+                selectedIndexPaths.append(indexPath)
+            }
+        }
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let tag = tagsDataSource.tags[indexPath.row]
+        
+        if let index = selectedIndexPaths.indexOf(indexPath) {
+            selectedIndexPaths.removeAtIndex(index)
+        } else {
+            selectedIndexPaths.append(indexPath)
+        }
+        
+        tableView.reloadRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+    }
+    
+    override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        if selectedIndexPaths.indexOf(indexPath) != nil {
+            cell.accessoryType = .Checkmark
+        } else {
+            cell.accessoryType = .None
+        }
     }
 }
