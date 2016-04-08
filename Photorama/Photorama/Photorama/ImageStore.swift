e2c1b230beb366f8ccd7092ff338a0827b2f0f03
Joel Reeves <jreeves@tonicdesign.com>
2016-04-08 10:46:59 -0400

    adding ImageStore from Homeowner project

---
+++ Photorama/Photorama/Photorama/ImageStore.swift
@@ -0,0 +1,64 @@
+//
+//  ImageStore.swift
+//  Homepwner
+//
+//  Created by Joel Reeves on 3/25/16.
+//  Copyright © 2016 Bromance Labs. All rights reserved.
+//
+
+import UIKit
+
+class ImageStore {
+    
+    let cache = NSCache()
+    
+    // adding image to the dictionary
+    func setImage(image: UIImage, forKey key: String) {
+        cache.setObject(image, forKey: key)
+        
+        // create full URL for image
+        let imageURL = imageURLForKey(key)
+        
+        // turn image into JPEG data
+        if let data = UIImageJPEGRepresentation(image, 0.5) {
+            // write it to full URL
+            data.writeToURL(imageURL, atomically: true)
+        }
+    }
+    
+    // retrieving image from the dictionary
+    func imageForKey(key: String) -> UIImage? {
+        if let existingImage = cache.objectForKey(key) as? UIImage {
+            return existingImage
+        }
+        
+        let imageURL = imageURLForKey(key)
+        
+        guard let imageFromDisk = UIImage(contentsOfFile: imageURL.path!) else {
+            return nil
+        }
+        
+        cache.setObject(imageFromDisk, forKey: key)
+        return imageFromDisk
+    }
+    
+    // deleting image from the dictionary
+    func deleteImageForKey(key: String) {
+        cache.removeObjectForKey(key)
+        
+        let imageURL = imageURLForKey(key)
+        
+        do {
+            try NSFileManager.defaultManager().removeItemAtURL(imageURL)
+        } catch let deleteError {
+            print("Error removing image from disk: \(deleteError)")
+        }
+        
+    }
+    
+    func imageURLForKey(key: String) -> NSURL {
+        let documentsDirectories = NSFileManager.defaultManager().URLsForDirectory((.DocumentDirectory), inDomains: .UserDomainMask)
+        let documentDirectory = documentsDirectories.first!
+        return documentDirectory.URLByAppendingPathComponent(key)
+    }
+}
