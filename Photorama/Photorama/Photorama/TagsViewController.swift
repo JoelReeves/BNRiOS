d1241e69216ac9b187edacd191b6c7f93c195948
Joel Reeves <jreeves@tonicdesign.com>
2016-04-08 11:10:25 -0400

    creating class to be used to select tags for a photo and adding new ones

---
+++ Photorama/Photorama/Photorama/TagsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TagsViewController.swift
+//  Photorama
+//
+//  Created by Joel Reeves on 4/8/16.
+//  Copyright © 2016 Bromance Labs. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class TagsViewController: UITableViewController {
+    
+    var store: PhotoStore!
+    var photo: Photo!
+    
+    var selectedIndexPaths = [NSIndexPath]()
+}
